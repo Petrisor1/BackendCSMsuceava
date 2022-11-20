6ceb00b89073d99829df7ad2dbeeee64d4669783
Petrisor1 <petrisorc65@gmail.com>
2022-11-20 22:35:25 +0200

    Handeled PUSH req

--- routes/csm.js
+++ routes/csm.js
@@ -1,21 +1,37 @@
 const express = require('express')
 const router = express.Router()
-
+const Csm =require('../models/csm')
 
 // Getting all
-router.get('/', (req,res) =>
+router.get('/', async(req,res) =>
 {
- res.send('Hello World')
+    try{
+        const csm = await Csm.find()
+        res.json(csm)
+    }catch(err)
+    {
+        res.status(500).json({message:err.message})
+    }
 })
 // Getting One
 router.get('/:id', (req,res) =>
 {
-    
+    res.send('post is ok');
 })
 // Creating one
-router.post('/', (req,res) =>
-{
-    
+router.post('/',async (req,res) =>
+{   const csm =new Csm({
+        name: req.body.name,
+        prenume: req.body.prenume
+    })
+    try{
+        const newCsm =await csm.save()
+        res.status(201).json(newCsm)
+    }catch(err)
+    {
+        res.status(400).json({message: err.message})
+    }
+   
 })
 // Updating one
 router.patch('/:id', (req,res) =>
